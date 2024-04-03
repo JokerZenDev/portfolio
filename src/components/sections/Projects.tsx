ad83615dbb063ca0a55dbbb0150d33aa3f5f2646
JokerZenDev <62612731+JokerZenDev@users.noreply.github.com>
2024-04-03 23:00:14 +0200

    feat(resume): add projects

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,27 +1,54 @@
 import Card from "../Card"
 import Section from "../Section"
 
-const ABOUT_TEXT =
-	"Hi, I'm Michele lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, consectetur voluptatem. Voluptatum molestiae impedit earum nihil nulla consequatur dolores molestias quis unde expedita? Commodi quo ipsa ex, quae quasi dolore."
+const PROJECTS = [
+	{
+		name: "IlBarchetto",
+		description: "Il Barchetto website built with NextJS 14 and Sanity.io CMS. This project is my first freelance project and I am proud of it.",
+		img: "/il-barchetto.png",
+		additionalInfo: [
+			{ name: "stack", value: "NextJS, Sanity.io, TS, TailwindCSS" },
+			{ name: "github", value: "https://github.com/JokerZenDev/il-barchetto", href: true },
+			// { name: "preview", value: "https://www.agriturismobarchetto.com/" },
+			{ name: "preview", value: "soon" },
+		],
+	},
+	{
+		name: "MontecarloETF",
+		description: "MonteCarloETF website built with NextJS and MongoDB. This is a personal project to simulate the results of an investment using ETF.",
+		img: "/montercarlo-etf.png",
+		additionalInfo: [
+			{ name: "stack", value: "NextJS, MongoDB, TS, TailwindCSS" },
+			{ name: "github", value: "https://github.com/JokerZenDev/montecarlo-etf", href: true },
+			{ name: "preview", value: "https://montecarlo-etf.jokerzen.dev", href: true },
+		],
+	},
+	{
+		name: "MyPortfolio",
+		description: "My portfolio website built with Vite and React TS.",
+		img: "/portfolio.png",
+		additionalInfo: [
+			{ name: "stack", value: "Vite, React, TS, TailwindCSS" },
+			{ name: "github", value: "https://github.com/JokerZenDev/portfolio", href: true },
+			{ name: "preview", value: "https://jokerzen.dev", href: true },
+		],
+	},
+]
 
 export default function Projects() {
 	return (
 		<Section isCentered={false} id='projects'>
-			<Card
-				title='Projects'
-				functionName="miky.dev"
-				variables={[
-					{ name: "stack", value: "NextJS, React, TS, TailwindCSS" },
-					{ name: "github", value: "https://github.com/miky", href: true },
-					{ name: "preview", value: "https://miky.dev", href: true },
-				]}
-				imgUrl='/hero.jpg'
-			>
-				{ABOUT_TEXT}
-			</Card>
-			<Card title='Projects' imgUrl='/hero.jpg'>
-				{ABOUT_TEXT}
-			</Card>
+			{PROJECTS.map((project, key) => (
+				<Card
+					key={key}
+					title="Projects"
+					functionName={project.name}
+					variables={project.additionalInfo}
+					imgUrl={project.img}
+				>
+					{project.description}
+				</Card>
+			))}
 		</Section>
 	)
 }
