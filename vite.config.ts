fc09e9e81db4372119cbd256dc6052a27fb04d21
JokerZenDev <62612731+JokerZenDev@users.noreply.github.com>
2024-10-18 00:02:03 +0200

    chore: support env var with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+export default defineConfig(
+  ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "")
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      define: {
+        "process.env.EMAILJS_PUBLIC_KEY": JSON.stringify(env.EMAILJS_PUBLIC_KEY),
+        "process.env.EMAILJS_SERVICE_ID": JSON.stringify(env.EMAILJS_SERVICE_ID),
+        "process.env.EMAILJS_TEMPLATE_ID": JSON.stringify(env.EMAILJS_TEMPLATE_ID),
+      },
+    }
+  }
+)
