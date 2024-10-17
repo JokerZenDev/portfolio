71ca04559e3dc9f083bb3c4449faeac2891a6080
JokerZenDev <62612731+JokerZenDev@users.noreply.github.com>
2024-10-17 16:33:41 +0200

    chore: add vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { PortfolioLanding } from './components/portfolio-landing'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <PortfolioLanding />
     </>
   )
